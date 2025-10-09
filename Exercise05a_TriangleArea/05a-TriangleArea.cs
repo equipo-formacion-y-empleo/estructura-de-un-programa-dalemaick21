e7890542be14b276fafed9f08adeaa55f766a146
dalemaick21 <valen198318@gmail.com>
2025-10-09 23:33:49 +0200

    corregido

--- Exercise05a_TriangleArea/05a-TriangleArea.cs
+++ Exercise05a_TriangleArea/05a-TriangleArea.cs
@@ -1,9 +1,12 @@
+
+double triangleBase = double.Parse(args.Length > 0 ? args[0] : "6,0");
+double triangleHeight = double.Parse(args.Length > 1 ? args[1] : "4,0");
+
+
 double CalculateTriangleArea()
 {
-    throw new NotImplementedException();
-}
+    return (triangleBase * triangleHeight) / 2;
+} 
 
-double triangleBase = double.Parse(args.Length > 0 ? args[0] : "6.0");
-double triangleHeight = double.Parse(args.Length > 1 ? args[1] : "4.0");
 
 Console.WriteLine($"Área del triángulo: {CalculateTriangleArea():F2}");
